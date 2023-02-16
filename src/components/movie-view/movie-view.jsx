4fefe6b1e1797ba1ed2d519880d51749c30907c4
cas-sav <94647032+cas-sav@users.noreply.github.com>
2023-02-16 15:34:18 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,28 +1,43 @@
+import React from 'react';
+import { Button, Row, Col, Container } from 'react-bootstrap';
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
-    <div>
-      <div>
-        <img className="w-100" src={movie.image} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.genre}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.director}</span>
-      </div>
-      <div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <img className="w-100" src={movie.image} alt={`Poster for ${movie.title}`} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Title: </span>
+          <span>{movie.title}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Genre: </span>
+          <span>{movie.genre}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Director: </span>
+          <span>{movie.director}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={onBackClick}>Back</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
