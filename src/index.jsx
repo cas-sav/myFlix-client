dbed87d456b14e6f9da85fb843d4797c328c854c
cas-sav <94647032+cas-sav@users.noreply.github.com>
2023-04-10 20:32:08 -0400

    styling

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { MainView } from "./components/main-view/main-view.jsx";
 import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle `./index.scss`
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main component (will eventually use all the others)
